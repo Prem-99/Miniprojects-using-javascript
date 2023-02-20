a7e7fad2841a1a977a248736fa80803e9c170eac
Prem-99 <115132125+Prem-99@users.noreply.github.com>
2023-02-20 17:44:45 +0530

    Add files via upload

---
+++ cards/script.js
@@ -0,0 +1,7 @@
+
+function LoadHome(){
+    location.replace("https://www.google.com/")
+}
+function LoadHome1(){
+    location.replace("https://www.myntra.com/login?referer=https%3A%2F%2Fwww.myntra.com%2Fmy%2Forders")
+}
